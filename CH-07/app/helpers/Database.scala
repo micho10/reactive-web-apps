6f315fd7d8082efc654a3c8cc59e49e79ca9bdbe
micho <gatin13@yahoo.com>
2017-01-23 15:11:06 +0900

    Added a helper to run the queries in a separate execution context

---
+++ CH-07/app/helpers/Database.scala
@@ -0,0 +1,22 @@
+package helpers
+
+import javax.inject.Inject
+
+import scala.concurrent.Future
+
+/**
+  * Created by carlos on 23/01/17.
+  */
+class Database @Inject() (db: play.api.db.Database) {
+
+  def query[A](block: DSLContext => A): Future[A] = Future {
+    db.withConnection { connection =>
+      // Creates the jOOQ DSLContext
+      val sql = DSL.using(connection, SQLDialect.POSTGRES_9_4)
+      // Invokes the function in the context of a database connection
+      block(sql)
+    }
+    // Explicitly passes the custom database ExecutionContext so that the Future will be executed against it
+  }(Contexts.database)
+
+}
