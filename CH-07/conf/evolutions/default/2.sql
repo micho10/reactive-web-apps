580b8417d6c140d1bdb020117628342e05c7e4f3
micho <gatin13@yahoo.com>
2017-01-30 15:09:15 +0900

    Modified the schema to represent the data

---
+++ CH-07/conf/evolutions/default/2.sql
@@ -0,0 +1,30 @@
+# ----!Ups
+
+CREATE TABLE  "twitter_user" (
+  id bigserial PRIMARY KEY,
+  created_on timestamp with time zone NOT NULL,
+  phone_number varchar NOT NULL,
+  twitter_user_name varchar NOT NULL
+);
+
+CREATE TABLE "mentions" (
+  id bigserial PRIMARY KEY,
+  tweet_id varchar NOT NULL,
+  user_id bigint NOT NULL,
+  created_on timestamp with time zone NOT NULL,
+  author_user_name varchar NOT NULL,
+  text varchar NOT NULL
+);
+
+CREATE TABLE "mention_subscriptions" (
+  id bigserial PRIMARY KEY,
+  created_on timestamp with time zone NOT NULL,
+  user_id bigint NOT NULL
+)
+
+
+# --- !Downs
+
+DROP TABLE "twitter_user"
+DROP TABLE "mentions"
+DROP TABLE "mention_subscriptions"
