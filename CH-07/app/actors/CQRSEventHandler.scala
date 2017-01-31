f2e6ee937c4bc4554901f6eac4ce826fd3ebf30e
micho <gatin13@yahoo.com>
2017-01-31 10:47:27 +0900

    Implement the CQRSEventHandler to write relevant events to the DB

--- CH-07/app/actors/CQRSEventHandler.scala
+++ CH-07/app/actors/CQRSEventHandler.scala
@@ -3,6 +3,8 @@ package actors
 import akka.actor.{Actor, ActorLogging}
 import play.api.db.Database
 
+import scala.util.Properties
+
 /**
   * Created by carlos on 30/01/17.
   */
@@ -13,6 +15,37 @@ class CQRSEventHandler(database: Database) extends Actor with ActorLogging {
     context.system.eventStream.subscribe(self, classOf[Event])
   }
 
-  override def receive = ???
+  override def receive = {
+    case UserRegistered(phoneNumber, userName, timestamp) => //TODO
+
+    case ClientEvent(phoneNumber, userName, MentionSubscribed(timestamp), _) =>
+      database.withTransaction { sql =>
+        // Creates an INSERT INTO ... SELECT statement
+
+        sql.insertInto(MENTION_SUBSCRIPTIONS)
+          .columns(
+            MENTION_SUBSCRIPTIONS.USER_ID,
+            MENTION_SUBSCRIPTIONS.CREATED_ON
+          )
+          .select(
+            // Creates the SELECT statement (the select method is provided by the wildcard import of the DSL class)
+            select(
+              TWITTER_USER.ID,
+              // Inserts the timestamp as a constant value using the value method
+              value(new Timestamp(timestamp.getMillis))
+            )
+            .from(TWITTER_USER)
+            .where(
+              TWITTER_USER.PHONE_NUMBER.equal(phoneNumber)
+              .and(
+                TWITTER_USER.TWITTER_USER_NAME.equal(userName)
+              )
+            )
+          ).execute()
+      }
+
+    case ClientEvent(phoneNumber, userName, MentionReceived(id, created_on, from, text, timestamp), _) =>
+      //TODO
+  }
 
 }
