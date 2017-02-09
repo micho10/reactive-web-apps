c10efaa64be6fbc73d1c1c28ad44210b7434caed
micho <gatin13@yahoo.com>
2017-02-09 18:34:29 +0900

    Added AJAX request

--- CH-10/app/controllers/Application.scala
+++ CH-10/app/controllers/Application.scala
@@ -1,11 +1,18 @@
 package controllers
 
+import javax.inject.Inject
+
+import play.api.Configuration
 import play.api.mvc._
 
-class Application extends Controller {
+class Application @Inject()(configuration: Configuration) extends Controller {
 
-  def index = Action {
+  def index = Action { implicit request =>
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def text = Action {
+    Ok(configuration.getString("text").getOrElse("Hello world!"))
   }
 
 }
--- CH-10/build.sbt
+++ CH-10/build.sbt
@@ -4,13 +4,15 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   jdbc,
   cache,
   ws,
-  specs2 % Test
+  specs2 % Test,
+  "org.webjars" %% "webjars-play" % "2.4.0-1",
+  "org.webjars" %  "jquery"       % "2.1.4"
 )
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
