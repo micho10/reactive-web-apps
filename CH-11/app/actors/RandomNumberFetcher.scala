97a73612bbe2f74a151997f8157d1790f47a1d1e
micho <gatin13@yahoo.com>
2017-02-15 13:18:47 +0900

    Added an actor to retrieve a random number using the random.org API and the corresponding actions to the controller

---
+++ CH-11/app/actors/RandomNumberFetcher.scala
@@ -0,0 +1,56 @@
+package actors
+
+import actors.RandomNumberFetcher.{FetchRandomNumber, RandomNumber}
+import akka.actor.{Actor, Props}
+import play.api.libs.json.{JsArray, Json}
+import play.api.libs.ws.WSClient
+import akka.pattern.pipe
+
+import scala.concurrent.Future
+
+/**
+  * Created by carlos on 15/02/17.
+  */
+class RandomNumberFetcher(ws: WSClient) extends Actor {
+
+  implicit val ec = context.dispatcher
+
+  override def receive = {
+    // Pipes the result of the future call to RANDOM.ORG to the sender, requesting a random number
+    case FetchRandomNumber(max) => fetchRandomNumber(max).map(RandomNumber) pipeTo sender()
+  }
+
+  def fetchRandomNumber(max: Int): Future[Int] =
+    ws.url("https://api.random.org/json-rpc/1/invoke")
+      .post(Json.obj(
+        "jsonrpc" -> "2.0",
+        "method"  -> "generateIntegers",
+        "params"  -> Json.obj(
+          // Passes the API key
+          "apiKey"      -> "...",
+          "n"           -> 1,
+          "min"         -> 0,
+          "max"         -> max,
+          "replacement" -> true,
+          "base"        -> 10
+        ),
+        "id"      -> 42
+      )).map { response =>
+      // Extracts the result in an unsafe manner to trigger a failure of the future if anything goes wrong
+      (response.json \ "result" \ "random" \ "data")
+        .as[JsArray]
+        .value
+        .head
+        .as[Int]
+    }
+
+}
+
+
+
+object RandomNumberFetcher {
+  def props(ws: WSClient) = Props(classOf[RandomNumberFetcher], ws)
+
+  case class FetchRandomNumber(max: Int)
+  case class RandomNumber(n: Int)
+}
--- CH-11/app/controllers/Application.scala
+++ CH-11/app/controllers/Application.scala
@@ -1,12 +1,46 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import javax.inject.Inject
 
-class Application extends Controller {
+import actors.RandomNumberFetcher
+import actors.RandomNumberFetcher.{FetchRandomNumber, RandomNumber}
+import akka.actor.ActorSystem
+import akka.util.Timeout
+import play.api.libs.ws.WSClient
+import play.api.mvc.{Action, Controller}
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
+
+/**
+  * Wires in dependencies using dependency injection
+  *
+  * @param ws
+  * @param ec
+  * @param system
+  */
+class Application @Inject()(ws: WSClient, ec: ExecutionContext, system: ActorSystem)  extends Controller {
+
+  implicit val executionContext = ec
+  // Sets timeout at 2 seconds
+  implicit val timeout = Timeout(2000.millis)
+
+  // Creates one single RandomNumberFetcher actor
+  val fetcher = system.actorOf(RandomNumberFetcher.props(ws))
+
+  def index = Action { implicit request =>
+    Ok(views.html.index())
+  }
+
+  def compute = Action.async { implicit request =>
+    (fetcher ? FetchRandomNumber(10)).map {
+      case RandomNumber(r) => Redirecct(routes.Application.index())
+          .flashing("result" -> s"The result is $r")
+
+      case other =>
+        // If we don't get a RandomNumber back, simply fails
+        InternalServerError
+    }
   }
 
 }
